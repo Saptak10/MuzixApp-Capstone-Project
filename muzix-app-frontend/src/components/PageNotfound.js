d178428600780018497d576aad6b764f54194db8
Saptak Chakraborty <saptak.bugatti20@gmail.com>
2023-10-16 11:22:15 +0000

    added pagenotfound and react router

---
+++ muzix-app-frontend/src/components/PageNotfound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageNotfound = () => {
+  return (
+    <div>
+      <h1>Page Not found</h1>
+      <img src="https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" width={200} height={200}/>
+    </div>
+  )
+}
+
+export default PageNotfound
